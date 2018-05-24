52413ececd946f7fdcd6675f2bf6c85d25dd4caa
backtake <stasiusoltysik@gmail.com>
2018-05-24 12:06:14 +0200

    Add set username and password functionality

--- src/main/java/com/codecool/charity/send/SendService.java
+++ src/main/java/com/codecool/charity/send/SendService.java
@@ -3,8 +3,12 @@ package com.codecool.charity.send;
 
 import com.codecool.charity.senders.SenderService;
 import com.codecool.charity.templates.TemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
@@ -13,36 +17,36 @@ import javax.mail.internet.MimeMessage;
 @Service
 public class SendService {
 
-
-    private JavaMailSender sender;
+    private JavaMailSenderImpl javaMailSender;
     private SenderService senderService;
     private TemplateService templateService;
 
 
-    public SendService(JavaMailSender sender,
+    public SendService(JavaMailSenderImpl javaMailSender,
                        SenderService senderService,
                        TemplateService templateService) {
-        this.sender = sender;
+        this.javaMailSender = javaMailSender;
         this.senderService = senderService;
         this.templateService = templateService;
     }
 
     public void sendEmail(Send send, String body) {
 
-        MimeMessage mail = sender.createMimeMessage();
+        javaMailSender.setUsername(send.getSender().getHostName());
+        javaMailSender.setPassword(send.getSender().getPassword());
+        MimeMessage mail = javaMailSender.createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(mail, true);
             helper.setTo(send.getReceiver());
-            helper.setReplyTo(send.getReceiver());
-            helper.setFrom(send.getSender().getHostName());
+            helper.setReplyTo(send.getSender().getHostName());
             helper.setSubject(send.getTemplate().getTitle());
-            helper.setText("template", true);
+            helper.setText(body, true);
 
         } catch (MessagingException e) {
             e.printStackTrace();
         }
 
-        sender.send(mail);
+        javaMailSender.send(mail);
     }
 
     public Send createSend(SendForm form){
