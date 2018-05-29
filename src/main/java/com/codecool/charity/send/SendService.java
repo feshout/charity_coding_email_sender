128d076722aabf92edb7ff55d87aecc603d500e8
backtake <stasiusoltysik@gmail.com>
2018-05-29 19:15:53 +0200

    Export functionality to separate private methods

--- src/main/java/com/codecool/charity/send/SendService.java
+++ src/main/java/com/codecool/charity/send/SendService.java
@@ -5,6 +5,9 @@ import com.codecool.charity.templates.TemplateService;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -15,20 +18,47 @@ public class SendService {
     private JavaMailSenderImpl javaMailSender;
     private SenderService senderService;
     private TemplateService templateService;
+    private TemplateEngine templateEngine;
 
     public SendService(JavaMailSenderImpl javaMailSender,
                        SenderService senderService,
-                       TemplateService templateService) {
+                       TemplateService templateService, TemplateEngine templateEngine) {
         this.javaMailSender = javaMailSender;
         this.senderService = senderService;
         this.templateService = templateService;
+        this.templateEngine = templateEngine;
     }
 
-    public void sendEmail(Send send, String body) {
+    void sendEmail(SendForm form, Model model) {
+        Send send = createSend(form);
+        Context context = getContext(send);
+        String body = templateEngine.process("display", context);
 
-        javaMailSender.setUsername(send.getSender().getHostName());
-        javaMailSender.setPassword(send.getSender().getPassword());
+        try {
+            prepareAndSendEmail(send, body);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        model.addAttribute("message", "Message sent with success");
+    }
+
+    void displayForm(Model model) {
+
+        SendForm sendForm = new SendForm();
+        model.addAttribute("sendForm", sendForm);
+        model.addAttribute("temps", templateService.getAll());
+    }
+
+    private void prepareAndSendEmail(Send send, String body) {
+
+        setUser(send);
         MimeMessage mail = javaMailSender.createMimeMessage();
+        setMimeMessageHelper(mail, send, body);
+        javaMailSender.send(mail);
+    }
+
+    private void setMimeMessageHelper(MimeMessage mail, Send send, String body) {
         try {
             MimeMessageHelper helper = new MimeMessageHelper(mail, true);
             helper.setTo(send.getReceiver());
@@ -39,11 +69,24 @@ public class SendService {
         } catch (MessagingException e) {
             e.printStackTrace();
         }
-
-        javaMailSender.send(mail);
     }
 
-    public Send createSend(SendForm form){
+    private void setUser(Send send) {
+        javaMailSender.setUsername(send.getSender().getHostName());
+        javaMailSender.setPassword(send.getSender().getPassword());
+    }
+
+
+    private Context getContext(Send send) {
+        Context context = new Context();
+        context.setVariable("header", send.getTemplate().getHeader());
+        context.setVariable("title", send.getTemplate().getTitle());
+        context.setVariable("description", send.getTemplate().getDescription());
+
+        return context;
+    }
+
+    private Send createSend(SendForm form){
         Send send = new Send();
 
         send.setReceiver(form.getTo());
@@ -52,4 +95,5 @@ public class SendService {
 
         return send;
     }
+
 }
