f337402842874958dff6f04c11f997d77e49cdf5
ragnarockie08 <ragnarockie08@gmail.com>
2018-06-14 11:35:39 +0200

    add deleteById method for repository

--- src/main/java/com/codecool/charity/templates/TemplateRepository.java
+++ src/main/java/com/codecool/charity/templates/TemplateRepository.java
@@ -9,6 +9,7 @@ public interface TemplateRepository extends CrudRepository<Template, Integer> {
     Template findById(Long id);
     Iterable<Template> findAll();
     Template findTemplateByHeader(String header);
+    Template deleteById(Long id);
 
 
 }
